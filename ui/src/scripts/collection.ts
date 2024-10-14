653e6d669090ecacdec2e8936159ebdd1baab4dd
devarogundade <devarogundade@gmail.com>
2024-10-14 14:56:21 +0100

    update collection ids

--- ui/src/scripts/collection.ts
+++ ui/src/scripts/collection.ts
@@ -7,7 +7,7 @@ export const tokens: Collection[] = [
         image: "/images/bayc.png",
         addresses: {
             10006: "0x626e574E9AE8860fF123458d4a38f1B9844FE9C1",
-            22: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+            22: "0xf78a0787746f607fa9d0fcfe842dbeee85b70466e3551c17628e3d68b276027b",
         }
     },
     {
@@ -16,7 +16,7 @@ export const tokens: Collection[] = [
         image: "/images/punks.png",
         addresses: {
             10006: "0x9A19e8574668dca5C4750eB642C614b7e5836cF4",
-            22: "0x14E0826c58f9C2a3a1B46b51F6d4705bCf0d6a22"
+            22: "0xaa45830fb72eae8ee1beb7bebd5b10935ce36066cbb79bf697f7e9c188ab5abf"
         }
     }
 ];
